8b8d8b3a965787aa7c2254bab7919e12c46e04de
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-11-26 21:57:30 -0600

    GLAB Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+    GALB_Array array = new GALB_Array();
+    array.arraydemoOne();
+    array.arraydemoTwo();
+    array.arraydemothree();
+    array.EnhancedForLoop();
+    array.MeanSDArray();
+    array.insertElementsr();
+    }
 }
